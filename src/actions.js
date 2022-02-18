7b5e7c4d99d2659cdcd602e18b06e48271243e38
Gandhyman <gandhycoder@gmail.com>
2022-02-18 07:46:31 -0700

    2nd commit

--- src/actions.js
+++ src/actions.js
@@ -1,20 +1,30 @@
 import axios from 'axios';
 
 
-export  function sendCart(data){
+export function sendCart(data) {
 
     return async (dispatch) => {
+
+        const config = {
+            url: 'https://demo-pasarela.herokuapp.com/mp',
+            method: 'post',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+                'Content-Type': 'application/json',
+            }, data: data
+        };
 
         try {
             console.log('Actions>>>>>>>', data)
             // axios.post('https://demo-pasarela.herokuapp.com/mp', data);
-            const payload = await axios.post('http://localhost:3001/mp', data);
+            const payload = await axios(config);
             console.log('Payload', payload.data.url)
             return dispatch({
                 type: 'PAGOMER',
                 payload: payload.data.url
             })
-            
+
         } catch (error) {
             console.log('Error>>>>>', error)
         }
